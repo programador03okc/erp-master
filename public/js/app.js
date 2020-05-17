c0759a1dee767349a0617161ccc2998dfde1dfa0
rocioeli <rocio_eli15@hotmail.com>
2020-05-17 13:11:36 -0500

    git ignore

--- public/js/app.js
+++ public/js/app.js
@@ -250,8 +250,8 @@ $(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\erp\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\erp\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\erp-master\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\erp-master\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
