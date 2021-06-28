451cd3c45fc93d68fe5f2ebffba73c5e810b042a
Desarrollo <Desarrollo@OKCCLOUD.COM>
2021-06-28 14:49:11 -0500

    npm run watch

--- public/js/app.js
+++ public/js/app.js
@@ -249,8 +249,8 @@ $(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp2\htdocs\erp-master\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp2\htdocs\erp-master\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\erp\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\erp\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
