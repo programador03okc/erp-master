cddb58dc351d860206d8fd7664cb903083b32d5e
rocioeli <rocio_eli15@hotmail.com>
2021-10-22 14:48:49 -0500

    ordenes pendientes versionado js

--- resources/views/almacen/guias/ordenesPendientes.blade.php
+++ resources/views/almacen/guias/ordenesPendientes.blade.php
@@ -175,21 +175,25 @@ Pendientes de Ingreso
 <script src="{{ asset('template/plugins/bootstrap-datepicker/locales/bootstrap-datepicker.es.min.js') }}"></script>
 
 <script src="{{ asset('js/almacen/guia/ordenesPendientes.js?')}}?v={{filemtime(public_path('js/almacen/guia/ordenesPendientes.js'))}}"></script>
-<script src="{{ asset('js/almacen/guia/transformacionesPendientes.js')}}"></script>
+<script src="{{ asset('js/almacen/guia/transformacionesPendientes.js?')}}?v={{filemtime(public_path('js/almacen/guia/transformacionesPendientes.js'))}}"></script>
+
 <script src="{{ asset('js/almacen/guia/ingresosProcesados.js')}}?v={{filemtime(public_path('js/almacen/guia/ingresosProcesados.js'))}}"></script>
-<script src="{{ asset('js/almacen/guia/ordenes_ver_detalle.js')}}"></script>
+<script src="{{ asset('js/almacen/guia/ordenes_ver_detalle.js')}}?v={{filemtime(public_path('js/almacen/guia/ordenes_ver_detalle.js'))}}"></script>
 <script src="{{ asset('js/almacen/guia/movimientoDetalle.js')}}?v={{filemtime(public_path('js/almacen/guia/movimientoDetalle.js'))}}"></script>
-<script src="{{ asset('js/almacen/guia/guia_com_create.js')}}"></script>
-<script src="{{ asset('js/almacen/guia/guia_com_cambio.js')}}"></script>
+<script src="{{ asset('js/almacen/guia/guia_com_create.js')}}?v={{filemtime(public_path('js/almacen/guia/guia_com_create.js'))}}"></script>
+<script src="{{ asset('js/almacen/guia/guia_com_cambio.js')}}?v={{filemtime(public_path('js/almacen/guia/guia_com_cambio.js'))}}"></script>
+
 <script src="{{ asset('js/almacen/guia/guia_com_det_series.js')}}?v={{filemtime(public_path('js/almacen/guia/guia_com_det_series.js'))}}"></script>
-<script src="{{ asset('js/almacen/guia/guia_com_det_series_edit.js')}}"></script>
-<script src="{{ asset('js/almacen/documentos/doc_com_create.js')}}"></script>
-<script src="{{ asset('js/almacen/documentos/doc_com_ver.js')}}"></script>
-<script src="{{ asset('js/almacen/transferencias/transferenciaCreate.js')}}"></script>
-<script src="{{ asset('js/tesoreria/facturacion/archivosMgcp.js')}}"></script>
+<script src="{{ asset('js/almacen/guia/guia_com_det_series_edit.js')}}?v={{filemtime(public_path('js/almacen/guia/guia_com_det_series_edit.js'))}}"></script>
+
+<script src="{{ asset('js/almacen/documentos/doc_com_create.js')}}?v={{filemtime(public_path('js/almacen/documentos/doc_com_create.js'))}}"></script>
+<script src="{{ asset('js/almacen/documentos/doc_com_ver.js')}}?v={{filemtime(public_path('js/almacen/documentos/doc_com_ver.js'))}}"></script>
+
+{{-- <script src="{{ asset('js/almacen/transferencias/transferenciaCreate.js')}}"></script> --}}
+<script src="{{ asset('js/tesoreria/facturacion/archivosMgcp.js')}}?v={{filemtime(public_path('js/tesoreria/facturacion/archivosMgcp.js'))}}"></script>
 <script src="{{ asset('js/logistica/mapeo/mapeoAsignarProducto.js')}}?v={{filemtime(public_path('js/logistica/mapeo/mapeoAsignarProducto.js'))}}"></script>
-<script src="{{ asset('js/almacen/guia/guia_transformacion_mapeo.js')}}"></script>
-<script src="{{ asset('js/almacen/guia/guia_com_create_transformacion.js')}}"></script>
+<script src="{{ asset('js/almacen/guia/guia_transformacion_mapeo.js')}}?v={{filemtime(public_path('js/almacen/guia/guia_transformacion_mapeo.js'))}}"></script>
+<script src="{{ asset('js/almacen/guia/guia_com_create_transformacion.js')}}?v={{filemtime(public_path('js/almacen/guia/guia_com_create_transformacion.js'))}}"></script>
 
 <script>
     $(document).ready(function() {
