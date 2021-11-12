73d9d68cc7e8938aa3cd378600d53990fe103e31
rocioeli <rocio_eli15@hotmail.com>
2021-11-12 12:11:35 -0500

    copia num docu a blade

--- public/js/logistica/orden/enviarOrdenSoftlink.js
+++ public/js/logistica/orden/enviarOrdenSoftlink.js
@@ -8,6 +8,10 @@ function enviarOrdenSoftlink() {
         dataType: 'JSON',
     }).done(function (response) {
         console.log(response);
+        console.log('response');
+        console.log(response.ocSoftlink.cabecera.cod_docu);
+        console.log(response.ocSoftlink.cabecera['cod_docu']);
+        $('[name=codigo_orden]').val(response.ocSoftlink.cabecera['cod_docu'] + response.ocSoftlink.cabecera['num_docu']);
         Lobibox.notify(response.tipo, {
             size: "mini",
             rounded: true,
