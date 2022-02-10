8a8ae0adbcdb8e213208070e26bef106a1590c84
Raúl Salinas <raulscodes@gmail.com>
2022-02-09 21:18:16 -0500

    exportable excel Listado de requerimientos

---
+++ resources/views/necesidades/reportes/view_requerimientos_bienes_servicios_export.blade.php
@@ -0,0 +1,39 @@
+<table>
+    <thead>
+        <tr>
+            <th>Prioridad</th>
+            <th>Requerimiento</th>
+            <th>Concepto</th>
+            <th>Fecha Registro</th>
+            <th>Fecha Entrega</th>
+            <th>Tipo Req.</th>
+            <th>Empresa</th>
+            <th>Grupo</th>
+            <th>División</th>
+            <th>Monto Total</th>
+            <th>Observacion</th>
+            <th>Creado por</th>
+            <th>Estado</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($requerimientos as $requerimiento)
+        <tr>
+            <td>{{ $requerimiento["priori"] }}</td>
+            <td>{{ $requerimiento["codigo"] }}</td>
+            <td>{{ $requerimiento["concepto"] }}</td>
+            <td>{{ $requerimiento["fecha_registro"] }}</td>
+            <td>{{ $requerimiento["fecha_entrega"] }}</td>
+            <td>{{ $requerimiento["tipo_requerimiento"] }}</td>
+            <td>{{ $requerimiento["razon_social"] }}</td>
+            <td>{{ $requerimiento["grupo"] }}</td>
+            <td>{{ $requerimiento["division"] }}</td>
+            <td>{{$requerimiento["simbolo_moneda"]}}{{ $requerimiento["monto_total"] }}</td>
+            <td>{{ $requerimiento["observacion"] }}</td>
+            <td>{{ $requerimiento["nombre_usuario"] }}</td>
+            <td>{{ $requerimiento["estado_doc"] }}</td>
+
+        </tr>
+        @endforeach
+    </tbody>
+</table>
