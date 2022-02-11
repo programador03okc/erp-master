a857d77d043d8642b7402f4591d2e9c7292d7d3e
rocioeli <rocio_eli15@hotmail.com>
2022-02-11 17:29:31 -0500

    ajustes login

--- public/js/app.js
+++ public/js/app.js
@@ -170,8 +170,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/raulsalinas/Developer/Web/Ok Computer/erp-agil/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/raulsalinas/Developer/Web/Ok Computer/erp-agil/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\erp-master\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\erp-master\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
