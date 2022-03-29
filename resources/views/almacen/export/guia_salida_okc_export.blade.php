63fbda72e0df3d0751bab5d64ecf725c4bad34fb
Raúl Salinas <raulscodes@gmail.com>
2022-03-29 18:21:05 -0500

    change marging top

--- resources/views/almacen/export/guia_salida_okc_export.blade.php
+++ resources/views/almacen/export/guia_salida_okc_export.blade.php
@@ -6,15 +6,6 @@
         <tr>
             <th></th>
         </tr>
-        <tr>
-            <th></th>
-        </tr>
-        <tr>
-            <th></th>
-        </tr>
-        <tr>
-            <th></th>
-        </tr>
     </thead>
     <tbody>
         <tr>
