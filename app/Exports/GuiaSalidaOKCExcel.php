9a146e3863cd44051f4b06fc5b8d53d7a26a77de
Raúl Salinas <raulscodes@gmail.com>
2022-03-30 16:06:34 -0500

    ajustes tamaño columnas para impresión

--- app/Exports/GuiaSalidaOKCExcel.php
+++ app/Exports/GuiaSalidaOKCExcel.php
@@ -30,26 +30,44 @@ class GuiaSalidaOKCExcel implements FromView, WithEvents
     {
         return [
             AfterSheet::class    => function (AfterSheet $event) {
-                $event->sheet->getDelegate()->getStyle('all')->getFont()->setSize(10);
+                $event->sheet->getDelegate()->getStyle('A1:U29')->getFont()->setSize(10);
+                // $sheet = $event->sheet->getDelegate();
+                // $sheet->getStyle('3')->getFont()->setSize(10);
 
-                $event->sheet->getDelegate()->getColumnDimension('A')->setWidth(3);
+                $event->sheet->getDelegate()->getColumnDimension('A')->setWidth(4);
                 $event->sheet->getDelegate()->getColumnDimension('B')->setWidth(4);
                 $event->sheet->getDelegate()->getColumnDimension('c')->setWidth(4);
                 $event->sheet->getDelegate()->getColumnDimension('D')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('E')->setWidth(4);
                 $event->sheet->getDelegate()->getColumnDimension('F')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('G')->setWidth(4);
                 $event->sheet->getDelegate()->getColumnDimension('H')->setWidth(4);
                 $event->sheet->getDelegate()->getColumnDimension('I')->setWidth(4);
                 $event->sheet->getDelegate()->getColumnDimension('J')->setWidth(4);
                 $event->sheet->getDelegate()->getColumnDimension('K')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('L')->setWidth(4);
                 $event->sheet->getDelegate()->getColumnDimension('M')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('N')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('O')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('P')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('Q')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('R')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('S')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('T')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('U')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('V')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('W')->setWidth(4);
                 $event->sheet->getStyle('H16:H20')->getAlignment()->setWrapText(true);
                 $event->sheet->getStyle('J7:J8')->getAlignment()->setWrapText(true);
                 $event->sheet->getStyle('D17')->getAlignment()->setWrapText(true);
+                $event->sheet->getDelegate()->getRowDimension(1)->setRowHeight(11);
+                $event->sheet->getDelegate()->getRowDimension(2)->setRowHeight(11);
                 $event->sheet->getDelegate()->getRowDimension(11)->setRowHeight(11);
 
                 $style = array(
                     'alignment' => array(
-                        'wrap' => true
+                        'wrap' => true,
+                        'font-size'=>10
                     )
                 );
                 $event->sheet->getStyle("H16:H20")->applyFromArray($style);
