24a25fce6e6c4fb7c569cc2d7db663dfeb5046f4
Raúl Salinas <raulscodes@gmail.com>
2022-03-30 12:57:07 -0500

    add td

--- resources/views/almacen/export/guia_salida_okc_export.blade.php
+++ resources/views/almacen/export/guia_salida_okc_export.blade.php
@@ -1,172 +1,192 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
 </head>
+
 <body>
-<table>
-    <thead>
-        <tr>
-            <th></th>
-        </tr>
-        <tr>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td colspan="9"></td>
-            <td colspan="4" style="text-align: center;">GR0000-000000</td>
-        </tr>
-        <tr>
-            <th></th>
-        </tr>
-        <tr>
-            <th></th>
-        </tr>
-        <tr>
-            <td colspan="2"></td>
-            <td colspan="2" style="text-align: center;">24/03/2022</td>
-        </tr>
-        <tr>
-            <td colspan="3"></td>
-            <td colspan="4" style="text-align: left;">00OK COMPUTER E.I.R.L.00</td>
-            <td colspan="3"></td>
-            <td colspan="4" style="text-align: left; word-wrap:break-word;vertical-align:top;">00MUNICIPALIDAD PROV000. CAR LOS F. FITZCARRALD000000000000000000000</td>
-        </tr>
-        <tr>
-            <td colspan="2"></td>
-            <td colspan="4" style="text-align: left;">CA LAS CASTAÑITAS N° 00127, 00SANISIDRO,0000LIMA00000000</td>
-            <td colspan="4"></td>
-            <td colspan="4" style="text-align: left; word-wrap:break-word;">0000000000000JR. FITZCARRALD Nº 50400- SAN 0LUIS, 00000000000 000FITZCARRALD</td>
-        </tr>
-        <tr>
-            <td colspan="2"></td>
-            <td colspan="2" style="text-align: left;">20519865476</td>
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+            </tr>
+            <tr>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td colspan="9"></td>
+                <td colspan="4" style="text-align: center;">GR0000-000000</td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td colspan="2"></td>
+                <td colspan="2" style="text-align: center;">24/03/2022</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td colspan="3"></td>
+                <td colspan="4" style="text-align: left;">00OK COMPUTER E.I.R.L.00</td>
+                <td colspan="4"></td>
+                <td colspan="4" style="text-align: left; word-wrap:break-word;vertical-align:top;">00MUNICIPALIDAD PROV000.</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td colspan="2"></td>
+                <td colspan="4" style="text-align: left;">CA LAS CASTAÑITAS N° 00127, 00SANISIDRO,0000LIMA00000000</td>
+                <td colspan="4"></td>
+                <td colspan="4" style="text-align: left; word-wrap:break-word;"> CAR LOS F. FITZCARRALD000000000000000000000</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td colspan="2"></td>
+                <td colspan="2" style="text-align: left;">20519865476</td>
+                <td colspan="5"></td>
+                <td colspan="4">0000000000000JR. FITZCARRALD Nº 50400- SAN 0LUIS, 00000000000 000FITZCARRALD</td>
 
-        </tr>
-        <tr>
-            <td colspan="3"></td>
-            <td colspan="3" style="text-align: left;">24/03/2022</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td colspan="2" style="text-align: center;">20202020098</td>
-        </tr>
-        <tr>
-            <td></td>
-        </tr>
-        <tr>
-            <td colspan="2"></td>
-            <td colspan="9" style="text-align: center;">INGRESAR NOMBRE DE TRANSPORTISTA00000000</td>
-            <td></td>
-            <td colspan="2" style="text-align: left;">LICENCIA</td>
-        </tr>
-        <tr>
-            <td colspan="2"></td>
-            <td colspan="4" style="text-align: left;">RUC TRANS</td>
-            <td colspan="5" style="text-align: left;">INGRESAR MARCA VEHICU</td>
-            <td></td>
-            <td colspan="2" style="text-align: left;">PLACA TRA</td>
-        </tr>
-        <tr>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;">0010966800</td>
-            <td colspan="2" style="text-align: left;">00'000,000</td>
-            <td colspan="2" style="text-align: left;">UND0000</td>
-            <td colspan="6" style="text-align: left;">COMPUTAD ORA PORTATIL</td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="6" style="text-align: left;">MARCA: HP</td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="6" style="text-align: left;">MODELO: 250 G8</td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="6" style="text-align: left;">NUMERO DE PARTE: 2 P 5 M 3 LT# ABM - W</td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="6" style="text-align: left; word-wrap:break-word;">COMP UTADORA PORTATIL: PROCESADOR: INTEL CORE I 7 - 1065 G 7 1 . 30 GHz RAM: 8 GB DDR 4 2666 333 MHz ALMACENAMIENTO: 1 TB HDD 5400 RPMPANTALLA: LCD CON RETROILUMINACION LED 15 . 6 " 1366 X 768 PIXELES LAN: SI WLAN: SI BLUETOOTH: SI V GA: NO HDMI: SI SIST. OPER: WINDOWS 10 P RO 64 BITS ESPAÑOL BATERIA: LI- ION 3 CELDAS PESO: 1 . 74 kg UNIDAD OPTICA: NO CAMARA WEB: SI SUITE OFIMATICA: NO G. F: 36 MESES ON- SITE</td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="6" style="text-align: left;"></td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="6" style="text-align: left;">S/ N:</td>
-        </tr>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td colspan="3"></td>
+                <td colspan="3" style="text-align: left;">24/03/2022</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td colspan="3" style="text-align: left;">20202020098</td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td colspan="2"></td>
+                <td colspan="9" style="text-align: center;">INGRESAR NOMBRE DE TRANSPORTISTA00000000</td>
+                <td colspan="2"></td>
+                <td colspan="2" style="text-align: left;">LICENCIA</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td colspan="2"></td>
+                <td colspan="6" style="text-align: left;">RUC TRANS</td>
+                <td colspan="5" style="text-align: left;">INGRESAR MARCA VEHICU</td>
+                <td colspan="2" style="text-align: left;">PLACA TRA</td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;">0010966800</td>
+                <td colspan="2" style="text-align: left;">00'000,000</td>
+                <td colspan="2" style="text-align: left;">UND0000</td>
+                <td colspan="9" style="text-align: left;">COMPUTAD ORA PORTATIL</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="9" style="text-align: left;">MARCA: HP</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="9" style="text-align: left;">MODELO: 250 G8</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="9" style="text-align: left;">NUMERO DE PARTE: 2 P 5 M 3 LT# ABM - W</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="9" style="text-align: left; word-wrap:break-word;">COMP UTADORA PORTATIL: PROCESADOR: INTEL CORE I 7 - 1065 G 7 1 . 30 GHz RAM: 8 GB DDR 4 2666 333 MHz ALMACENAMIENTO: 1 TB HDD 5400 RPMPANTALLA: LCD CON RETROILUMINACION LED 15 . 6 " 1366 X 768 PIXELES LAN: SI WLAN: SI BLUETOOTH: SI V GA: NO HDMI: SI SIST. OPER: WINDOWS 10 P RO 64 BITS ESPAÑOL BATERIA: LI- ION 3 CELDAS PESO: 1 . 74 kg UNIDAD OPTICA: NO CAMARA WEB: SI SUITE OFIMATICA: NO G. F: 36 MESES ON- SITE</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="9" style="text-align: left;"></td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="9" style="text-align: left;">S/ N:</td>
+            </tr>
 
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ01</td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ11</td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ21</td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ02</td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ12</td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ22</td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ03</td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ13</td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ23</td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ04</td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ14</td>
-            <td colspan="2" style="text-align: left;">CND127H0TZ24</td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-        </tr>
-        <tr>
-            <td colspan="3" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;">OC: 0059 </td>
-            <td colspan="2" style="text-align: left;"></td>
-            <td colspan="2" style="text-align: left;"></td>
-        </tr>
-    </tbody>
-</table>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ01</td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ11</td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ21</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ02</td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ12</td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ22</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ03</td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ13</td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ23</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ04</td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ14</td>
+                <td colspan="3" style="text-align: left;">CND127H0TZ24</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="3" style="text-align: left;"></td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="2" style="text-align: left;"></td>
+                <td colspan="3" style="text-align: left;">OC: 0059 </td>
+                <td colspan="3" style="text-align: left;"></td>
+                <td colspan="3" style="text-align: left;"></td>
+            </tr>
+        </tbody>
+    </table>
 </body>
+
 </html>
