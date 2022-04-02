fad5f317c88797b55107a8fda1cab34c207b797a
rocioeli <rocio_eli15@hotmail.com>
2022-04-02 11:19:42 -0500

    guia wherenotnull id_prod

--- app/Http/Controllers/Almacen/Movimiento/OrdenesPendientesController.php
+++ app/Http/Controllers/Almacen/Movimiento/OrdenesPendientesController.php
@@ -493,6 +493,7 @@ class OrdenesPendientesController extends Controller
                 ['log_det_ord_compra.estado', '!=', 7],
                 ['log_det_ord_compra.estado', '!=', 28]
             ])
+            ->whereNotNull('log_det_ord_compra.id_producto')
             ->get();
 
         return response()->json($detalle);
@@ -553,6 +554,7 @@ class OrdenesPendientesController extends Controller
             ->leftjoin('almacen.alm_prod', 'alm_prod.id_producto', '=', 'guia_com_det.id_producto')
             ->leftjoin('almacen.alm_und_medida', 'alm_und_medida.id_unidad_medida', '=', 'alm_prod.id_unidad_medida')
             ->where([['guia_com_det.id_guia_com', '=', $id_guia], ['guia_com_det.estado', '!=', 7]])
+            ->whereNotNull('log_det_ord_compra.id_producto')
             ->get();
 
         $lista = [];
