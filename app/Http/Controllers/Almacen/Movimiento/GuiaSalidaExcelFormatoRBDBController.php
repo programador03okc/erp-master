a923b0156e195659249938a018b756bab575a55c
Raúl Salinas <raulscodes@gmail.com>
2022-04-27 16:15:43 -0500

    guia de salida exce JEDR Y RBDB

--- app/Http/Controllers/Almacen/Movimiento/GuiaSalidaExcelFormatoRBDBController.php
+++ app/Http/Controllers/Almacen/Movimiento/GuiaSalidaExcelFormatoRBDBController.php
@@ -17,49 +17,80 @@ class GuiaSalidaExcelFormatoRBDBController extends Controller
         $guia=$data['guia'];
         $sheet->getDefaultColumnDimension()->setWidth(8, 'pt');
         $sheet->getRowDimension(1)->setRowHeight(60, 'pt');
-        $sheet->getRowDimension(3)->setRowHeight(45, 'pt');
+        $sheet->getRowDimension(3)->setRowHeight(26, 'pt');
 
-        $sheet->setCellValue('BH1', '');
+        $sheet->setCellValue('BM1', '');
 
         $sheet->setCellValue('AW2', 'GR'.($guia->serie.'-'.$guia->numero));
         $sheet->mergeCells('AW2:BF2');
 
-        $sheet->setCellValue('A4', $guia->fecha_emision);
-        $sheet->mergeCells('A4:G4');
+        $sheet->setCellValue('F4', $guia->fecha_emision);
+        $sheet->mergeCells('F4:L4');
 
-        $sheet->setCellValue('T4', $guia->fecha_emision);
-        $sheet->mergeCells('T4:Z4');
+        $sheet->setCellValue('Y4', $guia->fecha_emision);
+        $sheet->mergeCells('Y4:AE4');
 
-        $sheet->setCellValue('I6', $guia->cliente_razon_social);
-        $sheet->getStyle('I6')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
-        $sheet->mergeCells('I6:AB7');
-        $sheet->getStyle('I6')->getAlignment()->setWrapText(true);
+        $sheet->setCellValue('B7', $guia->punto_partida);
+        $sheet->getStyle('B7')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
+        $sheet->mergeCells('B7:AB8');
+        $sheet->getStyle('B7')->getAlignment()->setWrapText(true);
 
-        $sheet->setCellValue('AM5', $guia->punto_partida);
-        $sheet->getStyle('AM5')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
-        $sheet->mergeCells('AM5:BF6');
-        $sheet->getStyle('AM5')->getAlignment()->setWrapText(true);
 
-        $sheet->setCellValue('B9', $guia->punto_llegada);
-        $sheet->getStyle('B9')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
-        $sheet->mergeCells('B9:AB10');
-        $sheet->getStyle('B9')->getAlignment()->setWrapText(true);
+        $sheet->setCellValue('D9','DISTR');
+        $sheet->mergeCells('D9:J9');
 
-        $sheet->setCellValue('AU9', 'INGRESAR MARCA VEHICU');
-        $sheet->mergeCells('AU9:BF9');
+        $sheet->getStyle('O9')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        $sheet->setCellValue('O9','PROV');
+        $sheet->mergeCells('O9:U9');
 
-        $sheet->setCellValue('AU10', 'PLACA TRA');
-        $sheet->mergeCells('AU10:BF10');
+        $sheet->getStyle('Y9')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        $sheet->setCellValue('Y9','DPETO');
+        $sheet->mergeCells('Y9:AG9');
 
-        $sheet->setCellValue('G11', $guia->cliente_nro_documento);
-        $sheet->mergeCells('G11:P11');
-        $sheet->getStyle('G11')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
 
-        $sheet->setCellValue('AC11', 'NRO DNI');
-        $sheet->mergeCells('AC11:AH11');
+        $sheet->setCellValue('AK7', $guia->punto_llegada);
+        $sheet->getStyle('AK7')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
+        $sheet->mergeCells('AK7:BF8');
+        $sheet->getStyle('AK7')->getAlignment()->setWrapText(true);
 
-        $sheet->setCellValue('AZ11', 'LICENCIA');
-        $sheet->mergeCells('AZ11:BH11');
+
+        $sheet->setCellValue('AK9','DISTR');
+        $sheet->mergeCells('AK9:AQ9');
+
+        $sheet->getStyle('AW9')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        $sheet->setCellValue('AW9','PROV');
+        $sheet->mergeCells('AW9:BB9');
+
+        $sheet->getStyle('BH9')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        $sheet->setCellValue('BH9','DPETO');
+        $sheet->mergeCells('BH9:BL9');
+        
+
+        $sheet->setCellValue('G11', $guia->cliente_razon_social);
+        $sheet->getStyle('G11')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
+        $sheet->mergeCells('G11:AE12');
+        $sheet->getStyle('G11')->getAlignment()->setWrapText(true);
+
+
+        
+        $sheet->setCellValue('D13', $guia->cliente_nro_documento);
+        $sheet->mergeCells('D13:P13');
+        $sheet->getStyle('D13')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+
+        $sheet->setCellValue('V13', 'NRO DNI');
+        $sheet->mergeCells('V13:AA13');
+
+        $sheet->setCellValue('AU11', 'INGRESAR MARCA VEHICU');
+        $sheet->mergeCells('AU11:BF11');
+
+        $sheet->setCellValue('AU12', 'PLACA TRA');
+        $sheet->mergeCells('AU12:BF12');
+
+
+
+
+        $sheet->setCellValue('AT13', 'LICENCIA');
+        $sheet->mergeCells('AT13:BH13');
 
 
     }
@@ -70,7 +101,7 @@ class GuiaSalidaExcelFormatoRBDBController extends Controller
         $detalle = $data['detalle'];
         $pageMaxHeight = 1008;
         $ColumnaInicioItem = 1;
-        $filaInicioItem = 15;
+        $filaInicioItem = 16;
         $filaLimiteParaImprimir = 0;
         $filaLimiteMarcada = false;
 
@@ -78,39 +109,39 @@ class GuiaSalidaExcelFormatoRBDBController extends Controller
                 
             
             $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*1, $filaInicioItem, $detalle[$i]['codigo']);
-            $sheet->mergeCells(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*1).$filaInicioItem.':'.\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex(($ColumnaInicioItem*1)+4).$filaInicioItem);
+            $sheet->mergeCells(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*1).$filaInicioItem.':'.\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex(($ColumnaInicioItem*1)+5).$filaInicioItem);
             $sheet->getStyle(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*1).$filaInicioItem)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
 
-            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*6, $filaInicioItem, $detalle[$i]['cantidad']);
-            $sheet->mergeCells(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*6).$filaInicioItem.':'.\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex(($ColumnaInicioItem*6)+3).$filaInicioItem);
-            $sheet->getStyle(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*6).$filaInicioItem)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*8, $filaInicioItem, $detalle[$i]['cantidad']);
+            $sheet->mergeCells(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*8).$filaInicioItem.':'.\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex(($ColumnaInicioItem*8)+4).$filaInicioItem);
+            $sheet->getStyle(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*8).$filaInicioItem)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
 
-            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*11, $filaInicioItem, $detalle[$i]['abreviatura']);
-            $sheet->mergeCells(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*11).$filaInicioItem.':'.\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex(($ColumnaInicioItem*11)+4).$filaInicioItem);
-            $sheet->getStyle(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*11).$filaInicioItem)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*13, $filaInicioItem, $detalle[$i]['abreviatura']);
+            $sheet->mergeCells(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*13).$filaInicioItem.':'.\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex(($ColumnaInicioItem*13)+4).$filaInicioItem);
+            $sheet->getStyle(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*13).$filaInicioItem)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
 
-            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*18, $filaInicioItem, $detalle[$i]['descripcion']);
-            $sheet->mergeCells(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*18).$filaInicioItem.':'.\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex(($ColumnaInicioItem*18)+31).$filaInicioItem);
-            $sheet->getStyle(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*18).$filaInicioItem)->getAlignment()->setWrapText(true);
-            $sheet->getStyle(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*18).$filaInicioItem)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*22, $filaInicioItem, $detalle[$i]['descripcion']);
+            $sheet->mergeCells(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*22).$filaInicioItem.':'.\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex(($ColumnaInicioItem*22)+37).$filaInicioItem);
+            $sheet->getStyle(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*22).$filaInicioItem)->getAlignment()->setWrapText(true);
+            $sheet->getStyle(\PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($ColumnaInicioItem*22).$filaInicioItem)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
 
             $filaInicioItem++;
-            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*18, $filaInicioItem, 'CATEGORÍA: ');
+            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*22, $filaInicioItem, 'CATEGORÍA: ');
             $filaInicioItem++;
-            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*18, $filaInicioItem, 'MARCA: '.$detalle[$i]['marca']);
+            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*22, $filaInicioItem, 'MARCA: '.$detalle[$i]['marca']);
             $filaInicioItem++;
-            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*18, $filaInicioItem, 'MODELO: ');
+            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*22, $filaInicioItem, 'MODELO: ');
             $filaInicioItem++;
-            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*18, $filaInicioItem, 'NÚMERO DE PARTE: '.$detalle[$i]['part_number']);
+            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*22, $filaInicioItem, 'NÚMERO DE PARTE: '.$detalle[$i]['part_number']);
             $filaInicioItem++;
-            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*18, $filaInicioItem, 'S/N:');
+            $sheet->setCellValueByColumnAndRow($ColumnaInicioItem*22, $filaInicioItem, 'S/N:');
             $filaInicioItem++;
 
             $filaInicioItem++;
 
             $cantidadColumnasPorFilaSerie=3;
-            $anchoDeSerie=8;
-            $ColumnaInicioSerie=$ColumnaInicioItem*18;
+            $anchoDeSerie=14;
+            $ColumnaInicioSerie=$ColumnaInicioItem*22;
             $ii=0;
             for ($j=$idSerieInterrumpido; $j < count($detalle[$i]['series']) ; $j++) { 
                 $sheet->setCellValueByColumnAndRow($ColumnaInicioSerie+$ii, $filaInicioItem, $detalle[$i]['series'][$j]->serie);
@@ -146,6 +177,8 @@ class GuiaSalidaExcelFormatoRBDBController extends Controller
     {
         $spreadsheet = new Spreadsheet();
         $spreadsheet->getActiveSheet()->getPageMargins()->setLeft(0.35);
+        $spreadsheet->getActiveSheet()->getPageMargins()->setright(0.25);
+
         $spreadsheet->getDefaultStyle()->getFont()->setSize(10);
         $spreadsheet->getDefaultStyle()->getFont()->setName('Times New Roman');
         $spreadsheet->getActiveSheet()->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_PORTRAIT);
