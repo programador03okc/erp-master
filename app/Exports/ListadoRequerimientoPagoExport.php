806174259aa1e6506bb33f19e4a3f39b9afd3583
Dylan Quispe <programador01@okcomputer.com.pe>
2022-07-28 01:39:26 -0500

    cambios de exportacion de la lista de requerimientos de pago

--- app/Exports/ListadoRequerimientoPagoExport.php
+++ app/Exports/ListadoRequerimientoPagoExport.php
@@ -39,36 +39,57 @@ class ListadoRequerimientoPagoExport implements FromView
         $requerimientos = (new RequerimientoPagoController)->obtenerRequerimientosElaborados($meOrAll,$idEmpresa,$idSede,$idGrupo,$idDivision,$fechaRegistroDesde,$fechaRegistroHasta,$idEstado)->orderBy('fecha_registro','desc')->get();
 
         $data=[];
+        $requerimientosDetalle_array=[];
         foreach($requerimientos as $element){
 
 
             $requerimientosDetalle = (new RequerimientoPagoController)->obtenerRequerimientosElaboradosDetalle($element->id_requerimiento_pago);
-            var_dump($requerimientosDetalle);exit;
-            echo($requerimientosDetalle->fecha_autorizacion);exit;
-            $data[]=[
-                'priori'=> $element->prioridad,
-                'codigo'=> $element->codigo,
-                'concepto'=> $element->concepto,
-                'fecha_registro'=> $element->fecha_registro,
-                // 'fecha_entrega'=> $element->fecha_entrega,
-                'tipo_requerimiento'=> $element->descripcion_requerimiento_pago_tipo,
-                'razon_social'=> $element->descripcion_empresa_sede,
-                'grupo'=> $element->grupo,
-                'division'=> $element->division,
-                'descripcion_proyecto'=> $element->descripcion_proyecto,
-                'simbolo_moneda'=> $element->simbolo_moneda,
-                'monto_total'=> number_format($element->monto_total,2),
-                'observacion'=> $element->observacion,
-                'nombre_usuario'=> $element->usuario_nombre_corto,
-                'observacion'=> $element->observacion,
-                'estado_doc'=> $element->nombre_estado,
 
-                'fecha_autorizacion'=>$requerimientosDetalle->fecha_autorizacion!==null ?$requerimientosDetalle->fecha_autorizacion:''
+            $ordenesPago = (new RequerimientoPagoController)->ordenesPago($element->id_requerimiento_pago);
+            $pago_total = 0;
+            foreach ($ordenesPago as $key => $value) {
+                $pago_total = $pago_total + $value->total_pago;
+            }
+            $pago_total = round($pago_total,2);
 
-            ];
+            foreach ($requerimientosDetalle->detalle as $key => $value) {
+
+                $data[]=[
+                    'id_requerimiento_pago'=>$element->id_requerimiento_pago,
+                    'priori'=> $element->prioridad,
+                    'codigo'=> $element->codigo,
+                    'concepto'=> $element->concepto,
+                    'fecha_registro'=> $element->fecha_registro,
+                    // 'fecha_entrega'=> $element->fecha_entrega,
+                    'tipo_requerimiento'=> $element->descripcion_requerimiento_pago_tipo,
+                    'razon_social'=> $element->descripcion_empresa_sede,
+                    'grupo'=> $element->grupo,
+                    'division'=> $element->division,
+                    'descripcion_proyecto'=> $element->descripcion_proyecto,
+                    'simbolo_moneda'=> $element->simbolo_moneda,
+                    'monto_total'=> number_format($element->monto_total,2),
+                    'observacion'=> $element->observacion,
+                    'nombre_usuario'=> $element->usuario_nombre_corto,
+                    'observacion'=> $element->observacion,
+                    'estado_doc'=> $element->nombre_estado,
+
+                    'fecha_autorizacion'=>$requerimientosDetalle->fecha_autorizacion!==null ?$requerimientosDetalle->fecha_autorizacion:'',
+                    'pago_total'=>$pago_total,
+
+                    'c_costo'=> $value->partida,
+                    'partida'=> $value->c_costo
+                    // 'importe_pago'=>$ordenesPago->total_pago!==null ?$ordenesPago->total_pago:''
+
+                ];
+            }
+
+
         }
+        // var_dump($requerimientosDetalle_array[0]);exit;
+
         return view('necesidades.reportes.listado_requerimiento_pago_export_excel', [
-            'requerimientos' => $data
+            'requerimientos'        =>  $data,
+            'requerimientosDetalle' =>  $requerimientosDetalle_array
         ]);
     }
 }
