2d1e5b81172754dfe7628f3142c82a22ab808f63
rocioeli1 <programador01@okcomputer.com.pe>
2022-09-22 16:50:25 -0500

    cambios de menu de los acceos de almacen y necesidades

--- resources/views/layout/menu_necesidades.blade.php
+++ resources/views/layout/menu_necesidades.blade.php
@@ -1,7 +1,7 @@
 @section('sidebar')
 <ul class="sidebar-menu" data-widget="tree">
     <li><a href="{{route('necesidades.index')}}"><i class="fas fa-tachometer-alt"></i> <span>Necesidades</span></a></li>
-    @if(Auth::user()->tieneSubModulo(51))
+    {{-- @if(Auth::user()->tieneSubModulo(51)) --}}
     <li class=" treeview ">
         <a href="#">
             <i class="fas fa-file-prescription"></i> <span>Requerimiento logístico</span>
@@ -18,15 +18,15 @@
             @endif
         </ul>
     </li>
-    @endif
-    @if(Auth::user()->tieneSubModulo(52))
+    {{-- @endif
+    @if(Auth::user()->tieneSubModulo(52)) --}}
         @if(Auth::user()->tieneAplicacion(132))
     <li><a href="{{route('necesidades.pago.listado.index')}}"><i class="fas fa-file-invoice-dollar"></i> <span>Requerimiento de pago</span></a></li>
         @endif
 
-    @endif
-    @if(Auth::user()->tieneAplicacion(134))
+    {{-- @endif --}}
+    {{-- @if(Auth::user()->tieneAplicacion(134)) --}}
         <li><a href="{{route('necesidades.revisar-aprobar.listado.index')}}"><i class="fas fa-stamp"></i> <span>Revisar / aprobar</span></a></li>
-    @endif
+    {{-- @endif --}}
 </ul>
 @endsection
