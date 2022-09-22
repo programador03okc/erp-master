7dd5be992a0a1777a40be57e964bef54a5dba822
rocioeli1 <programador01@okcomputer.com.pe>
2022-09-22 16:53:03 -0500

    cambios de menu de los acceos de almacen y necesidades

--- resources/views/layout/menu_necesidades.blade.php
+++ resources/views/layout/menu_necesidades.blade.php
@@ -10,19 +10,19 @@
             </span>
         </a>
         <ul class="treeview-menu">
-            @if(Auth::user()->tieneAplicacion(102))
+            {{-- @if(Auth::user()->tieneAplicacion(102)) --}}
             <li><a href="{{route('necesidades.requerimiento.elaboracion.index')}}"><i class="far fa-circle fa-xs"></i> Crear / editar</a></li>
             @endif
-            @if(Auth::user()->tieneAplicacion(103))
+            {{-- @if(Auth::user()->tieneAplicacion(103)) --}}
             <li><a href="{{route('necesidades.requerimiento.listado.index')}}"><i class="far fa-circle fa-xs"></i> Listado</a></li>
-            @endif
+            {{-- @endif --}}
         </ul>
     </li>
     {{-- @endif
     @if(Auth::user()->tieneSubModulo(52)) --}}
-        @if(Auth::user()->tieneAplicacion(132))
+        {{-- @if(Auth::user()->tieneAplicacion(132)) --}}
     <li><a href="{{route('necesidades.pago.listado.index')}}"><i class="fas fa-file-invoice-dollar"></i> <span>Requerimiento de pago</span></a></li>
-        @endif
+        {{-- @endif --}}
 
     {{-- @endif --}}
     {{-- @if(Auth::user()->tieneAplicacion(134)) --}}
