ecb489edbafe2f648e9fc4a2e2509091c3205204
rocioeli1 <programador01@okcomputer.com.pe>
2022-10-07 11:44:30 -0500

    cambios de cobranza

--- resources/views/gerencial/cobranza/registro.blade.php
+++ resources/views/gerencial/cobranza/registro.blade.php
@@ -33,8 +33,8 @@ Cobranzas
         </div>
         <div class="box-body">
             <div class="row">
-                <div class="col-md-12 table-responsive">
-                    <table class="mytable table table-striped table-condensed table-bordered" id="listar-registros">
+                <div class="col-md-12 ">
+                    <table class="mytable table table-striped table-condensed table-bordered table-responsive" id="listar-registros">
                         <thead>
                             <tr>
                                 <th></th>
