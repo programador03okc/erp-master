7fbdc5089c275dd833e5c51677216b19f796ff64
rocioeli1 <programador01@okcomputer.com.pe>
2022-10-17 14:28:14 -0500

    cambios de power bi 2 archivos

--- resources/views/power-bi/cobranzas.blade.php
+++ resources/views/power-bi/cobranzas.blade.php
@@ -8,7 +8,7 @@
     <div class="box-body">
         <div class="row">
             <div class="col-md-12">
-                <iframe title="Reporte Cobranzas" width="100%" height="550px" src="https://app.powerbi.com/reportEmbed?reportId=1f9023d3-9e78-4639-8133-21bf96d58210&autoAuth=true&ctid=e5cdaa4d-557f-46cd-80ee-ff584f5964ac" 
+                <iframe title="Reporte Cobranzas" width="100%" height="550px" src="https://app.powerbi.com/reportEmbed?reportId=9fc906a8-46f0-4119-aa21-bd9ad629e351&autoAuth=true&ctid=e5cdaa4d-557f-46cd-80ee-ff584f5964ac"
                 frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </div>
