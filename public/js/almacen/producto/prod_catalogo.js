583e3bc23de478d5e69149acc6174706af77654d
rocioeli <rocio_eli15@hotmail.com>
2022-10-18 21:15:17 -0500

    catalogo de productos view report excel

--- public/js/almacen/producto/prod_catalogo.js
+++ public/js/almacen/producto/prod_catalogo.js
@@ -1,35 +1,45 @@
 $(function () {
     var vardataTables = funcDatatables();
-    var button_copiar   = (array_accesos.find(element => element === 37)?vardataTables[2][0]:[]),
-        button_excel    = (array_accesos.find(element => element === 39)?vardataTables[2][1]:[]),
-        button_pdf      = (array_accesos.find(element => element === 38)?vardataTables[2][2]:[]),
-        button_imprimir = (array_accesos.find(element => element === 40)?vardataTables[2][3]:[]);
+    var button_copiar = (array_accesos.find(element => element === 37) ? vardataTables[2][0] : []),
+        button_excel = (array_accesos.find(element => element === 39) ? vardataTables[2][1] : []),
+        button_pdf = (array_accesos.find(element => element === 38) ? vardataTables[2][2] : []),
+        button_imprimir = (array_accesos.find(element => element === 40) ? vardataTables[2][3] : []);
+
+    let botones = [];
+    botones.push({
+        text: ' Exportar Excel',
+        action: function () {
+            exportarProductos();
+        }, className: 'btn-success btnExportarProductos'
+    });
 
     $('#listaProductoCatalogo').dataTable({
         'dom': vardataTables[1],
-        'buttons': [button_copiar,button_excel,button_pdf,button_imprimir],
+        // 'buttons': [button_copiar, button_excel, button_pdf, button_imprimir],
         'language': vardataTables[0],
-        // 'processing': true,
+        buttons: botones,
         'ajax': 'listar_productos',
         'columns': [
             { 'data': 'id_producto' },
-            { 'data': 'clasif_descripcion' },
-            { 'data': 'cod_softlink' },
-            { 'data': 'tipo_descripcion' },
-            { 'data': 'cod_cat' },
-            { 'data': 'cat_descripcion' },
-            { 'data': 'cod_sub_cat' },
-            { 'data': 'subcat_descripcion' },
-            // { 'data': 'codigo' },
+            { 'data': 'part_number' },
             {
                 data: 'codigo',
                 'render': function (data, type, row) {
                     return `<a href="#" class="verProducto" data-id="${row['id_producto']}" >${row['codigo']}</a>`
                 }
             },
-            { 'data': 'part_number' },
+            { 'data': 'cod_softlink' },
             { 'data': 'descripcion' },
+            { 'data': 'notas' },
+            { 'data': 'simbolo' },
+            { 'data': 'series' },
             { 'data': 'abreviatura' },
+            { 'data': 'marca' },
+            { 'data': 'subcategoria' },
+            { 'data': 'categoria' },
+            { 'data': 'clasificacion' },
+            { 'data': 'fecha_registro' },
+            { 'data': 'nombre_corto' },
         ],
         'columnDefs': [{ 'aTargets': [0], 'sClass': 'invisible' }],
     });
@@ -44,3 +54,6 @@ $(function () {
 
     vista_extendida();
 });
+function exportarProductos() {
+    $('#formProductosExcel').trigger('submit');
+}
--- resources/views/almacen/producto/prod_catalogo.blade.php
+++ resources/views/almacen/producto/prod_catalogo.blade.php
@@ -10,21 +10,28 @@ Catálogo de Productos
     <div class="thumbnail" style="padding-left: 10px;padding-right: 10px;padding-top: 10px;">
         <div class="row">
             <div class="col-md-12">
+                <form id="formProductosExcel" method="POST" target="_blank"
+                    action="{{route('almacen.catalogos.catalogo-productos.catalogoProductosExcel')}}">
+                    @csrf()
+                </form>
                 <table class="mytable table table-condensed table-bordered table-okc-view" id="listaProductoCatalogo">
                     <thead>
                         <tr>
                             <th></th>
-                            <th>Clasificación</th>
-                            <th>Código Softlink</th>
-                            <th>Categoría</th>
-                            <th>Cód. Cat. Softlink</th>
-                            <th>Subcatgoría</th>
-                            <th>Cód. SubCat. Softlink</th>
-                            <th>Marca</th>
-                            <th>Código Agile</th>
                             <th>Part Number</th>
-                            <th>Descripción</th>
+                            <th>Código Agile</th>
+                            <th>Código Softlink</th>
+                            <th width="30%">Descripción</th>
+                            <th>Notas</th>
+                            <th>Moneda</th>
+                            <th>Control de Series</th>
                             <th>Unid. Med.</th>
+                            <th>Marca</th>
+                            <th>Subcatgoría</th>
+                            <th>Categoría</th>
+                            <th>Clasificación</th>
+                            <th>Fecha registro</th>
+                            <th>Registrado por</th>
                         </tr>
                     </thead>
                     <tbody></tbody>
