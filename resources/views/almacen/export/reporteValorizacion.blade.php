d070dd264949e29f317c4a3ba818def43a410514
rocioeli1 <programador01@okcomputer.com.pe>
2022-11-08 15:53:02 -0500

    cambios de stock vaorizado

--- resources/views/almacen/export/reporteValorizacion.blade.php
+++ resources/views/almacen/export/reporteValorizacion.blade.php
@@ -17,31 +17,55 @@
             <tr>
                 <th style="background-color: #f4f4f4; font-weight: bold;" width="10" rowspan="2">Código</th>
                 <th style="background-color: #f4f4f4; font-weight: bold;" width="10" rowspan="2">Código SoftLink</th>
+                <th style="background-color: #f4f4f4; font-weight: bold;" width="10" rowspan="2">Categoría</th>
+                <th style="background-color: #f4f4f4; font-weight: bold;" width="10" rowspan="2">Part Number</th>
                 <th style="background-color: #f4f4f4; font-weight: bold;" width="50" rowspan="2">Producto</th>
                 <th align="center" style="background-color: #f4f4f4; font-weight: bold;" width="15" rowspan="2">Stock</th>
-                <th align="center" style="background-color: #f4f4f4; font-weight: bold;" width="15" colspan="2">Valorizacion</th>
+                <th align="center" style="background-color: #f4f4f4; font-weight: bold;" width="15" colspan="2">Costo Promedio</th>
                 <th align="center" style="background-color: #f4f4f4; font-weight: bold;" width="15" colspan="2">Valorizacion</th>
             </tr>
             <tr>
-                <th align="center" style="background-color: #f4f4f4; font-weight: bold;">Soles</th>
-                <th align="center" style="background-color: #f4f4f4; font-weight: bold;">Dolares</th>
+                <th align="center" style="background-color: #f4f4f4; font-weight: bold;">Moneda</th>
+                <th align="center" style="background-color: #f4f4f4; font-weight: bold;">Costo U.</th>
                 <th align="center" style="background-color: #f4f4f4; font-weight: bold;" width="15">Soles</th>
                 <th align="center" style="background-color: #f4f4f4; font-weight: bold;" width="15">Dolares</th>
             </tr>
         </thead>
         <tbody>
+            @php
+                $total_soles=0;
+                $total_dolares=0;
+            @endphp
             @foreach ($data as $item)
                 <tr>
                     <td>{{ $item['codigo'] }}</td>
-                    <td>{{ $item['codigo_softlink'] }}</td>
+                    <td>{{ $item['cod_softlink'] }}</td>
+                    <td>{{ $item['categoria'] }}</td>
+                    <td>{{ $item['part_number'] }}</td>
                     <td>{{ $item['producto'] }}</td>
                     <td>{{ $item['stock'] }}</td>
-                    <td align="right">0.00</td>
-                    <td align="right">0.00</td>
-                    <td align="right">{{ $item['valorizacion_sol'] }}</td>
-                    <td align="right">{{ $item['valorizacion_dol'] }}</td>
+                    <td align="right">{{ $item['simbolo'] }}</td>
+                    <td align="right">{{ $item['costo_promedio'] }}</td>
+                    <td align="right">{{ $item['simbolo']!=='$' ? $item['valorizacion'] : '' }}</td>
+                    <td align="right">{{ $item['simbolo']==='$' ? $item['valorizacion'] : '' }}</td>
                 </tr>
+                @php
+                    $item['simbolo']!=='$' ? ($total_soles = floatval($total_soles) + floatval($item['valorizacion']))  : '';
+                    $item['simbolo']==='$' ? ($total_dolares = floatval($total_dolares) + floatval($item['valorizacion']))  : '';
+                @endphp
             @endforeach
+            <tr>
+                {{-- <td rowspan="2"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td align="right"></td> --}}
+                <td align="right"  colspan="8">TOTAL =</td>
+                <td align="right">{{ $total_soles }}</td>
+                <td align="right">{{ $total_dolares }}</td>
+            </tr>
         </tbody>
     </table>
 </body>
