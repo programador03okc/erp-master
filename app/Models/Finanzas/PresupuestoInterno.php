61d590dab2c0fbc1f7f6c1265b848268689e3cc4
rocioeli1 <programador01@okcomputer.com.pe>
2023-01-31 16:35:26 -0500

    cambios

--- app/Models/Finanzas/PresupuestoInterno.php
+++ app/Models/Finanzas/PresupuestoInterno.php
@@ -10,4 +10,35 @@ class PresupuestoInterno extends Model
     protected $table = 'finanzas.presupuesto_interno';
     protected $primaryKey = 'id_presupuesto_interno';
     public $timestamps = false;
+
+    public static function calcularTotalPresupuestoAnual($id_presupuesto_interno, $id_tipo_presupuesto)
+    {
+        $presupuesto_interno_destalle=array();
+        switch ($id_tipo_presupuesto) {
+            case 1:
+                $presupuesto_interno_destalle= PresupuestoInternoDetalle::where('id_presupuesto_interno',$id_presupuesto_interno)->where('id_tipo_presupuesto',1)->where('estado', 1)->orderBy('partida')->get();
+            break;
+
+            case 2:
+                $presupuesto_interno_destalle= PresupuestoInternoDetalle::where('id_presupuesto_interno',$id_presupuesto_interno)->where('id_tipo_presupuesto',2)->where('estado', 1)->orderBy('partida')->get();
+            break;
+            case 3:
+                $presupuesto_interno_destalle= PresupuestoInternoDetalle::where('id_presupuesto_interno',$id_presupuesto_interno)->where('id_tipo_presupuesto',3)->where('estado', 1)->orderBy('partida')->get();
+            break;
+        }
+        $enero      = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->enero));
+        $febrero    = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->febrero));
+        $marzo      = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->marzo ));
+        $abril      = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->abril));
+        $mayo       = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->mayo));
+        $junio      = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->junio));
+        $julio      = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->julio));
+        $agosto     = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->agosto));
+        $setiembre  = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->setiembre));
+        $octubre    = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->octubre));
+        $noviembre  = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->noviembre));
+        $diciembre  = floatval(str_replace(",", "", $presupuesto_interno_destalle[0]->diciembre));
+        $total      = $enero + $febrero + $marzo + $abril + $mayo + $junio + $julio + $agosto + $setiembre + $octubre + $noviembre + $diciembre;
+        return $total;
+    }
 }
