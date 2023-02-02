a4656c6c57694dbf3937e7ffe86a78a62c525997
rocioeli1 <programador01@okcomputer.com.pe>
2023-02-02 18:36:19 -0500

    cambio de sum a nivel de partida

--- app/Http/Controllers/Finanzas/Presupuesto/PresupuestoInternoController.php
+++ app/Http/Controllers/Finanzas/Presupuesto/PresupuestoInternoController.php
@@ -269,6 +269,7 @@ class PresupuestoInternoController extends Controller
         $costos= PresupuestoInternoDetalle::where('id_presupuesto_interno',$id)->where('id_tipo_presupuesto',2)->where('estado', 1)->orderBy('partida')->get();
         $gastos = PresupuestoInternoDetalle::where('id_presupuesto_interno',$id)->where('id_tipo_presupuesto',3)->where('estado', 1)->orderBy('partida')->get();
 
+        // return PresupuestoInterno::calcularTotalPresupuestoFilas($id,2);exit;
         return PresupuestoInterno::calcularTotalPresupuestoFilas($id,2);exit;
 
         return view('finanzas.presupuesto_interno.editar', compact('grupos','area','moneda','id','presupuesto_interno','ingresos','costos','gastos'));
