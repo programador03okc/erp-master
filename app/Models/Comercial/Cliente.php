00b9364eb0521724775d8653d25790bbabd608d3
EAlvarez7 <alvarez.nike7@gmail.com>
2023-03-03 10:26:31 -0500

    cambios cobranzas

--- app/Models/Comercial/Cliente.php
+++ app/Models/Comercial/Cliente.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    protected $table='comercial.com_cliente';
+    protected $table = 'comercial.com_cliente';
+    protected $primaryKey = 'id_cliente';
     protected $fillable = ['id_contribuyente', 'codigo', 'estado', 'fecha_registro'];
-    public $timestamps=false;
-    protected $primaryKey='id_cliente';
+    public $timestamps = false;
 
     public function contribuyente(){
         return $this->hasOne(Contribuyente::class, 'id_contribuyente', 'id_contribuyente');
