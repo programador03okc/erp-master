a0c36959287cb8139a019cfacd12ab1909ca55d0
rocioeli <rocio_eli15@hotmail.com>
2023-04-10 23:58:48 -0500

    ajustes campos cuadro costos

--- app/Http/Controllers/Finanzas/Presupuesto/PresupuestoController.php
+++ app/Http/Controllers/Finanzas/Presupuesto/PresupuestoController.php
@@ -108,11 +108,10 @@ class PresupuestoController extends Controller
                 WHERE registro_pago.id_oc = log_ord_compra.id_orden_compra
                 limit 1) AS fecha_pago"),
                 'requerimiento_pago_estado.descripcion as estado_pago',
-                'categoria_adjunto.descripcion as tipo_comprobante',
-                'adjuntos_logisticos.nro_comprobante',
-                'adjuntos_logisticos.fecha_emision as fecha_emision_comprobante'
+                DB::raw("CONCAT(doc_com.serie,'-',doc_com.numero) AS serie_numero"),
+                'cont_tp_doc.descripcion as tipo_comprobante',
+                'doc_com.fecha_emision as fecha_emision_comprobante'
                 // 'adjuntos_logisticos.monto_total as monto_total_comprobante'
-
                 // DB::raw("(SELECT sum(registro_pago.total_pago) FROM tesoreria.registro_pago
                 // WHERE registro_pago.id_oc = log_ord_compra.id_orden_compra) AS suma_pago"),
             )
@@ -122,22 +121,38 @@ class PresupuestoController extends Controller
             ->join('contabilidad.adm_contri', 'adm_contri.id_contribuyente', '=', 'adm_empresa.id_contribuyente')
             ->join('finanzas.presup_par', 'presup_par.id_partida', '=', 'alm_det_req.partida')
             ->join('finanzas.presup', 'presup.id_presup', '=', 'presup_par.id_presup')
-            // ->join('finanzas.presup_titu', 'presup_titu.codigo', '=', 'presup_par.cod_padre')
             ->join('logistica.log_ord_compra', 'log_ord_compra.id_orden_compra', '=', 'log_det_ord_compra.id_orden_compra')
-            // ->join('logistica.log_prove', 'log_prove.id_proveedor', '=', 'log_ord_compra.id_proveedor')
+            ->leftJoin('almacen.guia_com_det', function ($join) {
+                $join->on('guia_com_det.id_oc_det', '=', 'log_det_ord_compra.id_detalle_orden');
+                $join->where('guia_com_det.estado', '!=', 7);
+            })
+            // ->leftjoin('almacen.doc_com_det', 'doc_com_det.id_guia_com_det', '=', 'guia_com_det.id_guia_com_det')
+            ->leftJoin('almacen.doc_com_det', function ($join) {
+                $join->on('doc_com_det.id_guia_com_det', '=', 'guia_com_det.id_guia_com_det');
+                $join->where('doc_com_det.estado', '!=', 7);
+            })
+            // ->leftjoin('almacen.doc_com', 'doc_com.id_doc_com', '=', 'doc_com_det.id_doc')
+            ->leftJoin('almacen.doc_com', function ($join) {
+                $join->on('doc_com.id_doc_com', '=', 'doc_com_det.id_doc');
+                $join->where('doc_com.estado', '!=', 7);
+            })
+            // ->leftjoin('contabilidad.cont_tp_doc', 'cont_tp_doc.id_tp_doc', '=', 'doc_com.id_tp_doc')
+            ->leftJoin('contabilidad.cont_tp_doc', function ($join) {
+                $join->on('cont_tp_doc.id_tp_doc', '=', 'doc_com.id_tp_doc');
+                $join->where('cont_tp_doc.estado', '=', 1);
+            })
             // ->leftJoin('finanzas.presup_titu', function ($join) {
             //     $join->on('presup_titu.codigo', '=', 'presup_par.cod_padre');
             //     $join->where('presup_titu.id_presup', '=', 'presup_par.id_presup');
             // })
-            // ->join('contabilidad.adm_contri as prove', 'prove.id_contribuyente', '=', 'log_prove.id_contribuyente')
             ->join('tesoreria.requerimiento_pago_estado', 'requerimiento_pago_estado.id_requerimiento_pago_estado', '=', 'log_ord_compra.estado_pago')
             ->join('configuracion.sis_moneda', 'sis_moneda.id_moneda', '=', 'log_ord_compra.id_moneda')
             ->join('logistica.log_prove', 'log_prove.id_proveedor', '=', 'log_ord_compra.id_proveedor')
             ->join('contabilidad.adm_contri as proveedor', 'proveedor.id_contribuyente', '=', 'log_prove.id_contribuyente')
             ->join('tesoreria.registro_pago', 'registro_pago.id_oc', '=', 'log_det_ord_compra.id_orden_compra')
             ->leftjoin('almacen.alm_und_medida', 'alm_und_medida.id_unidad_medida', '=', 'alm_det_req.id_unidad_medida')
-            ->leftjoin('logistica.adjuntos_logisticos', 'adjuntos_logisticos.id_orden', '=', 'log_ord_compra.id_orden_compra')
-            ->leftjoin('logistica.categoria_adjunto', 'categoria_adjunto.id_categoria_adjunto', '=', 'adjuntos_logisticos.categoria_adjunto_id')
+            // ->leftjoin('logistica.adjuntos_logisticos', 'adjuntos_logisticos.id_orden', '=', 'log_ord_compra.id_orden_compra')
+            // ->leftjoin('logistica.categoria_adjunto', 'categoria_adjunto.id_categoria_adjunto', '=', 'adjuntos_logisticos.categoria_adjunto_id')
 
             ->where([
                 ['presup.id_presup', '=', $id_presupuesto],
@@ -153,8 +168,6 @@ class PresupuestoController extends Controller
             ->select(
                 'requerimiento_pago_detalle.*',
                 'requerimiento_pago.codigo',
-                'requerimiento_pago.concepto',
-                'requerimiento_pago.fecha_registro',
                 'adm_contri.razon_social',
                 'sis_moneda.simbolo',
                 'alm_und_medida.abreviatura',
@@ -162,17 +175,16 @@ class PresupuestoController extends Controller
                 'presup_par.descripcion as partida_descripcion',
                 DB::raw("(SELECT presup_titu.descripcion FROM finanzas.presup_titu
                 WHERE presup_titu.codigo = presup_par.cod_padre
-                and presup_titu.id_presup = presup_par.id_presup) AS titulo_descripcion"),
-                DB::raw("(SELECT registro_pago.fecha_pago FROM tesoreria.registro_pago
-                WHERE registro_pago.id_requerimiento_pago = requerimiento_pago.id_requerimiento_pago
-                limit 1) AS fecha_pago"),
+                and presup_titu.id_presup = presup_par.id_presup limit 1) AS titulo_descripcion"),
+                // DB::raw("(SELECT registro_pago.fecha_pago FROM tesoreria.registro_pago
+                // WHERE registro_pago.id_requerimiento_pago = requerimiento_pago.id_requerimiento_pago
+                // limit 1) AS fecha_pago"),
                 'rrhh_perso.apellido_paterno','rrhh_perso.apellido_materno','rrhh_perso.nombres',
                 'rrhh_perso.nro_documento as nro_documento_persona',
                 'requerimiento_pago_categoria_adjunto.descripcion as tipo_comprobante',
                 'requerimiento_pago_adjunto.nro_comprobante',
                 'requerimiento_pago_adjunto.fecha_emision as fecha_emision_comprobante'
                 // 'requerimiento_pago_adjunto.monto_total as monto_tota_comprobante'
-
                 // DB::raw("(SELECT sum(registro_pago.total_pago) FROM tesoreria.registro_pago
                 // WHERE registro_pago.id_requerimiento_pago = requerimiento_pago.id_requerimiento_pago) AS suma_pago"),
             )
@@ -183,17 +195,20 @@ class PresupuestoController extends Controller
             ->join('contabilidad.adm_contri', 'adm_contri.id_contribuyente', '=', 'adm_empresa.id_contribuyente')
             ->join('finanzas.presup_par', 'presup_par.id_partida', '=', 'requerimiento_pago_detalle.id_partida')
             ->join('configuracion.sis_moneda', 'sis_moneda.id_moneda', '=', 'requerimiento_pago.id_moneda')
-            ->leftjoin('rrhh.rrhh_trab', 'rrhh_trab.id_trabajador', '=', 'requerimiento_pago.id_trabajador')
-            ->leftjoin('rrhh.rrhh_postu', 'rrhh_postu.id_postulante', '=', 'rrhh_trab.id_postulante')
-            ->leftjoin('rrhh.rrhh_perso', 'rrhh_perso.id_persona', '=', 'rrhh_postu.id_persona')
+            ->join('rrhh.rrhh_trab', 'rrhh_trab.id_trabajador', '=', 'requerimiento_pago.id_trabajador')
+            ->join('rrhh.rrhh_postu', 'rrhh_postu.id_postulante', '=', 'rrhh_trab.id_postulante')
+            ->join('rrhh.rrhh_perso', 'rrhh_perso.id_persona', '=', 'rrhh_postu.id_persona')
             // ->leftJoin('finanzas.presup_titu', function ($join) {
             //     $join->on('presup_titu.codigo', '=', 'presup_par.cod_padre');
             //     $join->where('presup_titu.id_presup', '=', 'presup_par.id_presup');
             // })
             ->leftjoin('almacen.alm_und_medida', 'alm_und_medida.id_unidad_medida', '=', 'requerimiento_pago_detalle.id_unidad_medida')
-            ->leftjoin('tesoreria.requerimiento_pago_adjunto', 'requerimiento_pago_adjunto.id_requerimiento_pago', '=', 'requerimiento_pago.id_requerimiento_pago')
+            // ->leftjoin('tesoreria.requerimiento_pago_adjunto', 'requerimiento_pago_adjunto.id_requerimiento_pago', '=', 'requerimiento_pago.id_requerimiento_pago')
+            ->leftJoin('tesoreria.requerimiento_pago_adjunto', function ($join) {
+                    $join->on('requerimiento_pago_adjunto.id_requerimiento_pago', '=', 'requerimiento_pago.id_requerimiento_pago');
+                    $join->where('requerimiento_pago_adjunto.id_estado', '!=', 7);
+                })
             ->leftjoin('tesoreria.requerimiento_pago_categoria_adjunto', 'requerimiento_pago_categoria_adjunto.id_requerimiento_pago_categoria_adjunto', '=', 'requerimiento_pago_adjunto.id_categoria_adjunto')
-
             ->where([
                 ['presup_par.id_presup', '=', $id_presupuesto],
                 // ['registro_pago.estado', '!=', 7],
--- resources/views/finanzas/presupuestos/export/cuadroGastosExcel.blade.php
+++ resources/views/finanzas/presupuestos/export/cuadroGastosExcel.blade.php
@@ -12,24 +12,23 @@
                 <th style="background-color: #cccccc;text-align:center;" width="15"><b>Mes</b></th>
                 <th style="background-color: #cccccc;text-align:center;" width="15"><b>Año</b></th>
                 <th style="background-color: #cccccc;text-align:center;" width="15"><b>Fecha</b></th>
-                {{-- <th style="background-color: #cccccc;" width="25"><b>Empresa</b></th> --}}
                 <th style="background-color: #cccccc;" width="15"><b>Cod.Req.</b></th>
-                <th style="background-color: #cccccc;" width="18"><b>Cuenta (Partida)</b></th>
+                <th style="background-color: #cccccc;" width="25"><b>Cuenta (Partida)</b></th>
                 <th style="background-color: #cccccc;" width="40"><b>Cuenta (Sub Partida)</b></th>
                 <th style="background-color: #cccccc;" width="20"><b>Tipo documento</b></th>
-                <th style="background-color: #cccccc;" width="10"><b>Número - serie</b></th>
-                <th style="background-color: #cccccc;" width="10"><b>Fecha emisión</b></th>
-                <th style="background-color: #cccccc;" width="10"><b>Ruc/DNI</b></th>
+                <th style="background-color: #cccccc;" width="20"><b>Número - serie</b></th>
+                <th style="background-color: #cccccc;" width="15"><b>Fecha emisión</b></th>
+                <th style="background-color: #cccccc;" width="15"><b>Ruc/DNI</b></th>
                 <th style="background-color: #cccccc;" width="40"><b>Proveedor o Persona asignada</b></th>
                 <th style="background-color: #cccccc;" width="8"><b>Cant.</b></th>
                 <th style="background-color: #cccccc;" width="10"><b>Unid.</b></th>
                 <th style="background-color: #cccccc;" width="50"><b>Descripción</b></th>
-                <th style="background-color: #cccccc;" width="18"><b>Mnd.</b></th>
+                <th style="background-color: #cccccc;" width="8"><b>Mnd.</b></th>
                 <th style="background-color: #cccccc;" width="18"><b>Unit.</b></th>
                 <th style="background-color: #cccccc;" width="18"><b>SubTotal</b></th>
                 <th style="background-color: #cccccc;" width="18"><b>IGV</b></th>
                 <th style="background-color: #cccccc;" width="18"><b>P.Compra</b></th>
-                <th style="background-color: #cccccc;" width="18"><b>Fecha pago</b></th>
+                {{-- <th style="background-color: #cccccc;" width="18"><b>Fecha pago</b></th> --}}
                 <th style="background-color: #cccccc;" width="18"><b>Estado pago</b></th>
             </tr>
         </thead>
@@ -47,7 +46,7 @@
                 <td>{{$d->titulo_descripcion}}</td>
                 <td>{{$d->partida_descripcion}}</td>
                 <td>{{$d->tipo_comprobante}}</td>
-                <td>{{$d->nro_comprobante}}</td>
+                <td>{{$d->serie_numero}}</td>
                 <td>{{$d->fecha_emision_comprobante}}</td>
                 <td>{{($d->nro_documento_proveedor !=null ? $d->nro_documento_proveedor : ($request->nro_documento_persona != null ? $request->nro_documento_persona :''))}}</td>
                 <td>{{$d->proveedor_razon_social}}</td>
@@ -59,7 +58,7 @@
                 <td>{{$d->subtotal}}</td>
                 <td>{{$d->subtotal*0.18}}</td>
                 <td>{{$d->subtotal + ($d->subtotal*0.18)}}</td>
-                <td>{{$d->fecha_pago}}</td>
+                {{-- <td>{{$d->fecha_pago}}</td> --}}
                 <td>{{$d->estado_pago}}</td>
             </tr>
             @endforeach
@@ -71,6 +70,10 @@
                 <td>{{$d->codigo}}</td>
                 <td>{{$d->titulo_descripcion}}</td>
                 <td>{{$d->partida_descripcion}}</td>
+                <td>{{$d->tipo_comprobante}}</td>
+                <td>{{$d->nro_comprobante}}</td>
+                <td>{{$d->fecha_emision_comprobante}}</td>
+                <td></td>
                 <td>{{$d->apellido_paterno.' '.$d->apellido_materno.' '.$d->nombres}}</td>
                 <td>{{$d->cantidad}}</td>
                 <td>{{$d->abreviatura}}</td>
@@ -80,7 +83,7 @@
                 <td>{{$d->subtotal}}</td>
                 <td>{{$d->subtotal*0.18}}</td>
                 <td>{{$d->subtotal + ($d->subtotal*0.18)}}</td>
-                <td>{{$d->fecha_pago}}</td>
+                {{-- <td>{{$d->fecha_pago}}</td> --}}
                 <td>{{$d->estado_pago}}</td>
             </tr>
             @endforeach
