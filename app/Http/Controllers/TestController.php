547fbde1c676b16f1d2584a9d0411f21a01f5f2b
EAlvarez7 <alvarez.nike7@gmail.com>
2023-04-17 17:55:15 -0500

    limpiar test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,7 +14,7 @@ class TestController extends Controller
         $data = [];
 
         foreach ($lista as $key) {
-            $nombre = trim(str_replace('•', '', utf8_decode($key->descripcion_adicional)));
+            $nombre = trim(strtoupper(str_replace('•', '', utf8_decode($this->quitar_tildes($key->descripcion_adicional)))));
 
             $update = OrdenCompraDetalle::find($key->id_detalle_orden);
                 $update->descripcion_adicional = $nombre;
@@ -23,5 +23,46 @@ class TestController extends Controller
             $data[] = ['antes' => $key->descripcion_adicional, 'despues' => $nombre];
         }
         return response()->json($data, 200);
+    }
+
+    public function quitar_tildes($cadena)
+    {
+		//Reemplazamos la A y a
+		$cadena = str_replace(
+			array('Á', 'À', 'Â', 'Ä', 'á', 'à', 'ä', 'â', 'ª'),
+			array('A', 'A', 'A', 'A', 'a', 'a', 'a', 'a', 'a'),
+			$cadena
+		);
+		//Reemplazamos la E y e
+		$cadena = str_replace(
+			array('É', 'È', 'Ê', 'Ë', 'é', 'è', 'ë', 'ê'),
+			array('E', 'E', 'E', 'E', 'e', 'e', 'e', 'e'),
+			$cadena
+		);
+		//Reemplazamos la I y i
+		$cadena = str_replace(
+			array('Í', 'Ì', 'Ï', 'Î', 'í', 'ì', 'ï', 'î'),
+			array('I', 'I', 'I', 'I', 'i', 'i', 'i', 'i'),
+			$cadena
+		);
+		//Reemplazamos la O y o
+		$cadena = str_replace(
+			array('Ó', 'Ò', 'Ö', 'Ô', 'ó', 'ò', 'ö', 'ô'),
+			array('O', 'O', 'O', 'O', 'o', 'o', 'o', 'o'),
+			$cadena
+		);
+		//Reemplazamos la U y u
+		$cadena = str_replace(
+			array('Ú', 'Ù', 'Û', 'Ü', 'ú', 'ù', 'ü', 'û'),
+			array('U', 'U', 'U', 'U', 'u', 'u', 'u', 'u'),
+			$cadena
+		);
+		//Reemplazamos la N, n, C y c
+		$cadena = str_replace(
+			array('Ñ', 'ñ', 'Ç', 'ç'),
+			array('N', 'n', 'C', 'c'),
+			$cadena
+		);
+		return $cadena;
     }
 }
